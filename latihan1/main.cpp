2cd72f17d8e30602fe52cb8ec08262e0187c24f1
AsepHendra <AsepHendra67.com>
2018-10-07 13:08:16 -0700

    selamat anda berhasil

---
+++ latihan1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int p, L, luas;
+    p=10;
+    L=5;
+    luas= p * L;
+    cout << "luasnya adalah :" << luas << endl;
+    return 0;
+}
